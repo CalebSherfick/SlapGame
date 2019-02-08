705a5638cdfdc25116e06435be6060935e4adee8
Caleb Sherfick <calebsherfick@gmail.com>
2019-02-08 15:27:27 -0700

    added punch & kick as well as dynamic name and a hit counter

--- apps/game.js
+++ apps/game.js
@@ -1,21 +1,43 @@
 console.log('start')
 
-var health = 100;
+let health = 100;
+let hits = 0
+let name = "Tom"
 
 
 
 function slap() {
   if (health > 0) {
-    health--
+    health--;
+    hits++
   }
-  alert(health)
+  update()
+}
+
+function punch() {
+  if (health > 0) {
+    health = health - 5;
+    hits++
+  }
+  update()
+}
+
+function kick() {
+  if (health > 0) {
+    health = health - 10;
+    hits++
+  }
+  update()
 }
 
 
+function update() {
+  document.getElementById("name").innerText = ` ${name}`;
+  document.getElementById("health").innerText = ` ${health.toString()}`;
+  document.getElementById("hits").innerText = ` ${hits.toString()}`;
 
-
-
-
+}
+update();
 
 
 
